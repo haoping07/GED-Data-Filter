ca66084c571f715f7f7fd83a726c6b5bbb436d17
SHIH-HAO LO <howard9900090@gmail.com>
2019-02-07 14:45:50 -0500

    LS
    individual

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,12 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import indi.individual;
+
 
 public class Main {
     public static void main(String[] args) throws IOException {
---
+++ src/indi/individual.java
@@ -0,0 +1,91 @@
+package indi;
+
+public class individual {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	}
+	
+	public individual(String id) {
+		this.id = id;
+		name = "";
+		sex = "";
+		Birthday = "";
+		Deathday = "";
+		children = new String[1];
+		spouse = new String[1];
+		isdead = false;
+	}
+	
+	public void update(int level , String tag , String content) {
+		switch(tag.toUpperCase()) {
+		    case "NAME":
+		    	this.name = content;
+			break;
+			
+		    case "SEX":
+		    	this.sex = content;
+			break;
+			
+		    case "BIRT":
+		    	if(checkdate(content , this.Deathday)) {
+		    		this.Birthday = content;
+		    	}
+			break;
+			
+		    case "DEAT":
+		    	if(checkdate(this.Birthday , content)) {
+		    		updatedeath(content);
+		    	}
+			break;
+			
+		    case "CHIL":
+		    	addchild(content);
+			break;
+			
+		    case "SPOU":
+		    	addmerrage(content);
+			break;
+			
+			default:
+		}
+	}
+	
+	private void addmerrage(String family_id) {
+		String[] temp = new String[this.spouse.length+1];
+		for(int x = 0 ; x < this.spouse.length ; x++) {
+			temp[x] = this.spouse[x];
+		}
+		
+		temp[temp.length-1] = family_id;
+		this.spouse = temp;
+	}
+
+	protected boolean checkdate(String date1 , String date2) {
+		return true;
+	}
+	
+	private void updatedeath(String date) {
+		this.isdead = true;
+		this.Deathday = date;
+	}
+	
+	private void addchild(String child_id) {
+		String[] temp = new String[this.children.length+1];
+		for(int x = 0 ; x < this.children.length ; x++) {
+			temp[x] = this.children[x];
+		}
+		
+		temp[temp.length-1] = child_id;
+		this.children = temp;
+	}
+	
+	public String id;
+	public String name;
+	public String sex;
+	public String Birthday;
+	public String Deathday;
+	public String[] children;
+	public String[] spouse;
+	public boolean isdead;
+}
