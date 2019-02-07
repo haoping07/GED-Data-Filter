5a1e48c172f5a36a71cdce29da5355de1823ba6e
gjc921019 <guojiacheng.bupt@gmail.com>
2019-02-07 13:53:03 -0500

    GJ
    Family class

---
+++ src/Family.java
@@ -0,0 +1,15 @@
+public class Family {
+
+    public:
+    String familyID;
+    String INDI_husband;
+    String INDI_wife;
+    String INDI_child;
+    String marrDate;
+    String divoDate;
+
+    public static void main(String[] args){
+
+    }
+
+}
