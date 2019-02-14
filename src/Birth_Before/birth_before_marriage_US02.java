61dcb21601dca05b51292204bf772ebe21597d5e
SHIH-HAO LO <howard9900090@gmail.com>
2019-02-14 15:01:36 -0500

    LS

--- src/Birth_Before/birth_before_marriage_US02.java
+++ src/Birth_Before/birth_before_marriage_US02.java
@@ -1,14 +1,14 @@
 //Auther:Song Xu
-package birth_before;
+package Birth_Before;
 
-import datecheck.checkdate_US01.java;
+import datecheck.checkdate_US01;
 
-public class birth_before_marriage_US03 {
+public class birth_before_marriage_US02 {
 	public boolean birth_before_death(String birth_date,String marriage_date){
 		String[] brith=birth_date.split(" ");
 		String[] marriage=marriage_date.split(" ");
 
-		if(checkdate_US01.checkyear(birth[2] , marriage[2])) {
+		if(checkdate_US01.checkyear(brith[2] , marriage[2])) {
 			if(checkdate_US01.checkmonth(brith[1] , marriage[1])) {
 				if(checkdate_US01.checkday(brith[0] , marriage[0])) {
 					return true;
