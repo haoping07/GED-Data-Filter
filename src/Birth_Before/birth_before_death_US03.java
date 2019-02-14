4284e41d83412eaed5bb274275b18940fd56da54
Sam <936485078@qq.com>
2019-02-14 16:39:26 -0500

    SX
    US03 US02

--- src/Birth_Before/birth_before_death_US03.java
+++ src/Birth_Before/birth_before_death_US03.java
@@ -4,10 +4,20 @@ package Birth_Before;
 import datecheck.checkdate_US01;
 
 public class birth_before_death_US03 {
-    public boolean birth_before_death(String birth_date,String death_date){
+	public static void main(String[] args) {
+		System.out.println(birth_before_death("28 JAN 2019","28 JAN 2019"));
+		System.out.println(birth_before_death("17 JAN 2019","28 JAN 2019"));
+		System.out.println(birth_before_death("28 JAN 2019","28 FEB 2019"));
+		System.out.println(birth_before_death("28 JAN 2018","28 JAN 2019"));
+		System.out.println(birth_before_death("28 JAN 2019","18 JAN 2019"));
+		System.out.println(birth_before_death("28 FEB 2019","28 JAN 2019"));
+		System.out.println(birth_before_death("28 JAN 2019","28 JAN 2018"));
+		
+	}
+    public static boolean birth_before_death(String birth_date,String death_date){
         String[] brith=birth_date.split(" ");
         String[] death=death_date.split(" ");
-
+        
         if(checkdate_US01.checkyear(death[2], brith[2])) {
             if(checkdate_US01.checkmonth(death[1], brith[1])) {
                 if(checkdate_US01.checkday(death[0], brith[0])) {
@@ -15,7 +25,7 @@ public class birth_before_death_US03 {
                 }
             }
         }
-            return false;
+        return false;
     }
 }
 
--- src/Birth_Before/birth_before_marriage_US02.java
+++ src/Birth_Before/birth_before_marriage_US02.java
@@ -4,7 +4,17 @@ package Birth_Before;
 import datecheck.checkdate_US01;
 
 public class birth_before_marriage_US02 {
-	public boolean birth_before_death(String birth_date,String marriage_date){
+	public static void main(String[] args) {
+		System.out.println(birth_before_marriage("28 JAN 2019","28 JAN 2019"));
+		System.out.println(birth_before_marriage("17 JAN 2019","28 JAN 2019"));
+		System.out.println(birth_before_marriage("28 JAN 2019","28 FEB 2019"));
+		System.out.println(birth_before_marriage("28 JAN 2018","28 JAN 2019"));
+		System.out.println(birth_before_marriage("28 JAN 2019","18 JAN 2019"));
+		System.out.println(birth_before_marriage("28 FEB 2019","28 JAN 2019"));
+		System.out.println(birth_before_marriage("28 JAN 2019","28 JAN 2018"));
+		
+	}
+	public static boolean birth_before_marriage(String birth_date,String marriage_date){
 		String[] birth=birth_date.split(" ");
 		String[] marriage=marriage_date.split(" ");
 
