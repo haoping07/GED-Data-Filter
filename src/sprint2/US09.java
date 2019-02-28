54771523a9d10e9cdf19ccf25c7b005bde81f9fb
SHIH-HAO LO <slo@stevens.edu>
2019-02-28 16:21:20 -0500

    LS

--- src/sprint2/US09.java
+++ src/sprint2/US09.java
@@ -14,58 +14,144 @@ public class US09 {
 	
 	public void Birth_before_death_of_parents(ArrayList<individual> people , ArrayList<Family> families) {
 		individual person = null;
+		Family thisfamily = null;
 		individual father = null;
 		individual mother = null;
-		int fid = 0;
-		int mid = 0;
 		
 		for(int x = 0 ; x < people.size() ; x++) {
 			person = people.get(x);
-			fid = get_family_by_id(families , person.id);
-			father = people.get(fid);
+			thisfamily = get_family_by_id(families , person.children.get(0));
+			if(thisfamily == null) {
+				System.out.println("Debug messages(US09)[WARNING]: No parent family is found for person " + person.id);
+				continue;
+			}
+			else{
+				father = get_person_by_id(people , thisfamily.husband);
+				if(father == null) {
+					System.out.println("Debug messages(US09)[WARNING]: No father is found for person " + person.id);
+					continue;
+				}
+				else {
+					mother = get_person_by_id(people , thisfamily.wife);
+					if(mother == null) {
+						System.out.println("Debug messages(US09)[WARNING]: No mother is found for person " + person.id);
+						continue;
+					}
+					else {
+						if(father.isdead) {
+							if(!checkfather(person.Birthday , father.Deathday)) {
+								System.out.println("Debug messages(US09) [**ILLEGAL**]: birth 9 months after death of father ::" + person.id);
+							}
+						}
+						if(mother.isdead) {
+							if(!checkmother(person.Birthday , mother.Deathday)) {
+								System.out.println("Debug messages(US09) [**ILLEGAL**]: birth after death of mother ::" + person.id);
+							}
+						}
+
+					}
+				}
+			}
 		}
 	}
 
 
-	public static boolean checkday(String nowday , String inday){
-		if(stoi(inday) > stoi(nowday)) return false;
-		else return true;
-	}
-	public static boolean checkdayeq(String nowday , String inday){
-		if(stoi(inday) == stoi(nowday)) return true;
-		else return false;
-	}
-	public static boolean checkmontheq(String nowmonth , String inmonth){
+	public boolean checkfather(String child, String father) {
+		// TODO Auto-generated method stub
 		String[] month = {" " , "JAN" , "FEB" , "MAR" , "APR" , "MAY" , "JUN" , "JUL" , "AUG" , "SEP" , "OCT" , "NOV" , "DEC"};
-		int in = inarridx(month , inmonth);
-		//System.out.println("in"+in);
-		//System.out.println("now:"+nowmonth);
-		if(in == -1) return false;
-		if(in == stoi(nowmonth)) return true;
-		else return false;
+		String[] childarr = child.split(" ");
+		String[] fatherarr = father.split(" ");
+		int childmonth = inarridx(month , childarr[1]);
+		int fathermonth = inarridx(month , fatherarr[1]);
+		int childyear = stoi(childarr[2]);
+		int fatheryear = stoi(fatherarr[2]);
+		int childday = stoi(childarr[0]);
+		int fatherday = stoi(fatherarr[0]);
+		
+		if(fathermonth + 9 <= 12) {
+			if(childyear > fatheryear) {
+				return false;
+			}
+			else if(childyear == fatheryear) {
+				if(childmonth > fathermonth) {
+					return false;
+				}
+				else if(childmonth == fathermonth) {
+					if(childday <= fatherday) {
+						return true;
+					}
+					else {
+						return false;
+					}
+				}
+				else {
+					return true;
+				}
+			}
+			else {
+				return true;
+			}
+		}
+		else{
+			if(childyear > fatheryear+1) {
+				return false;
+			}
+			else if(childyear == fatheryear+1) {
+				if(childmonth > fathermonth+9-12) {
+					return false;
+				}
+				else if(childmonth == fathermonth+9-12) {
+					if(childday > fatherday) {
+						return false;
+					}
+					else {
+						return true;
+					}
+				}
+				else {
+					return true;
+				}
+			}
+			else {
+				return true;
+			}
+		}
+		
 	}
 	
-	public static boolean checkmonth(String premonth , String aftmonth){
+	public boolean checkmother(String child, String mother) {
 		String[] month = {" " , "JAN" , "FEB" , "MAR" , "APR" , "MAY" , "JUN" , "JUL" , "AUG" , "SEP" , "OCT" , "NOV" , "DEC"};
-		int pre = inarridx(month , premonth);
-		int aft = inarridx(month , aftmonth);
-		//System.out.println("pre"+pre);
-		//System.out.println("aft:"+aft);
-		if(pre == -1 || aft == -1) return false;
+		String[] childarr = child.split(" ");
+		String[] motherarr = mother.split(" ");
+		int childmonth = inarridx(month , childarr[1]);
+		int mothermonth = inarridx(month , motherarr[1]);
+		int childyear = stoi(childarr[2]);
+		int motheryear = stoi(motherarr[2]);
+		int childday = stoi(childarr[0]);
+		int motherday = stoi(motherarr[0]);
+		
+		if(childyear > motheryear) {
+			return false;
+		}
+		else if(childyear == motheryear) {
+			if(childmonth > mothermonth) {
+				return false;
+			}
+			else if(childmonth == mothermonth) {
+				if(childday > motherday) {
+					return false;
+				}
+				else {
+					return true;
+				}
+			}
+			else {
+				return true;
+			}
+		}
 		else {
-			if(aft > pre) return false;
-			else return true;
+			return true;
 		}
-	}
-	
-	public static boolean checkyear(String nowyear , String inyear){
-		if(stoi(inyear) > stoi(nowyear)) return false;
-		else return true;
-	}
-	
-	public static boolean checkyeareq(String nowyear , String inyear){
-		if(stoi(inyear) == stoi(nowyear)) return true;
-		else return false;
 	}
 	
 	public static int stoi(String input) {
@@ -83,12 +169,21 @@ public class US09 {
 		return -1;
 	}
 	
-	public int get_family_by_id(ArrayList<Family> families , String target) {
+	public Family get_family_by_id(ArrayList<Family> families , String target) {
 		for(int x = 0 ; x < families.size() ; x++) {
 			if(families.get(x).familyID.equals(target)) {
-				return x;
+				return families.get(x);
 			}
 		}
-		return -1;
+		return null;
+	}
+	
+	public individual get_person_by_id(ArrayList<individual> people , String target) {
+		for(int x = 0 ; x < people.size() ; x++) {
+			if(people.get(x).id.equals(target)) {
+				return people.get(x);
+			}
+		}
+		return null;
 	}
 }
