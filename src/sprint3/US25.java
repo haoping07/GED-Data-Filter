fa93b2e2498799a43f234c642dc8b499e1a6d752
SHIH-HAO LO <slo@stevens.edu>
2019-03-30 17:52:17 -0400

    Add us25,27

--- src/sprint3/US25.java
+++ src/sprint3/US25.java
@@ -10,30 +10,30 @@ import java.util.HashSet;
 import Mainprogram.*;
 
 public class US25 {
-
+	public static ArrayList<Family> allFamilies;
+	public static ArrayList<individual> allPeople;
+	public static String err;
+	
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		output_format data;
 		try {
-			unique_first_name();
+			data = Main.main_output();
+			unique_first_name(data.Families , data.People);
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
 	
-	public static void unique_first_name() throws IOException {
-		output_format data = Main.main_output();
-		ArrayList<individual> allPeople = data.People;
-		ArrayList<Family> allFamilies = data.Families;
+	public static void unique_first_name(ArrayList<Family> families , ArrayList<individual> people){
+		allPeople = people;
+		allFamilies = families;
 		ArrayList<individual> fam = new ArrayList<>();
 		ArrayList<String> tmp = new ArrayList<>();
 		individual itmp;
-		HashSet<String> names = new HashSet<>();
-		String[] nametmp = new String[2];
 		for(int x = 0 ; x < allFamilies.size() ; x++) {
 			fam.clear();
 			tmp.clear();
-			names.clear();
 			tmp = allFamilies.get(x).children;
 			for(int y = 0 ; y < tmp.size() ; y++) {
 				itmp = searchbyId(allPeople , tmp.get(y));
@@ -41,16 +41,27 @@ public class US25 {
 			}
 			fam.add(searchbyId(allPeople , allFamilies.get(x).husband));
 			fam.add(searchbyId(allPeople , allFamilies.get(x).wife));
-			for(int a = 0 ; a < fam.size() ; a++) {
-				nametmp = fam.get(a).name.split(" ");
-				if(!names.contains(nametmp[0])) {
-					names.add(nametmp[0]);
-				}
-				else {
-					System.out.println("Debug messages(US25) [**ILLEGAL**]: name " + nametmp[0] + "is not unique in family " + allFamilies.get(x).familyID);
-				}
+			if(!check_unique(fam)) {
+				System.out.println("Debug messages(US25) [**ILLEGAL**]: name \"" + err + "\" is not unique in family " + allFamilies.get(x).familyID);
 			}
 		}
+	}
+	
+	public static boolean check_unique(ArrayList<individual> member) {
+		HashSet<String> names = new HashSet<>();
+		String[] nametmp = new String[2];
+		for(int a = 0 ; a < member.size() ; a++) {
+			nametmp = member.get(a).name.split(" ");
+			if(!names.contains(nametmp[0])) {
+				names.add(nametmp[0]);
+			}
+			else {
+				err = new String();
+				err = nametmp[0];
+				return false;
+			}
+		}
+		return true;
 	}
 	
     public static individual searchbyId(ArrayList<individual> people , String target) {
